69c86a58ed86a087fa4c0c83e1f235f8aaf970a8
Gluttony-Meow <87435118+Gluttony-Meow@users.noreply.github.com>
2022-11-09 08:17:03 +0700

    9/11

--- Assets/Scripts/GridSquare.cs
+++ Assets/Scripts/GridSquare.cs
@@ -1,18 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameEvent;
 
 public class GridSquare : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int SquareIndex { get; set; }
+    private AlphabetData.LetterData _normaLetterData;
+    private AlphabetData.LetterData _selectedLetterData;
+    private AlphabetData.LetterData _correctLetterData;
+
+    private SpriteRenderer displayedImage;
+    private bool _selected;
+    private bool _clicked;
+    private int _index = -1;
+    private bool _correct;
+
+    public void SetIndex(int Index)
     {
-        
+        _index = Index;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int GetIndex()
     {
-        
+        return _index; 
     }
+    private void Start()
+    {
+        _selected = false;
+        _clicked = false;
+        _correct = false;
+        displayedImage = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        GameEvent.OnEnableSquareSelection += OnEnableSquareSelection;
+        GameEvent.OnDisableSquareSelection += OnDisableSquareSelection;
+        GameEvent.OnSelectSquare += SelectSquare;
+    }
+
+    private void OnDisable()
+    {
+        GameEvent.OnEnableSquareSelection -= OnEnableSquareSelection;
+        GameEvent.OnDisableSquareSelection -= OnDisableSquareSelection;
+        GameEvent.OnSelectSquare -= SelectSquare;
+    }
+
+    public void OnEnableSquareSelection()
+    {
+        _clicked = true;
+        _selected = false;
+    }
+
+    public void OnDisableSquareSelection()
+    {
+        _selected = false;
+        _clicked = false;
+
+        if (_correct == true)
+            displayedImage.sprite = _correctLetterData.image;
+        else
+            displayedImage.sprite = _normaLetterData.image; 
+    }
+
+    private void SelectSquare(Vector3 position)
+    {
+        if (this.gameObject.transform.position == position)
+            displayedImage.sprite = _selectedLetterData.image;
+    }
+    public void SetSprite(AlphabetData.LetterData normaLetterData, AlphabetData.LetterData selectedLetterData,
+        AlphabetData.LetterData correctLetterData)
+    {
+        _normaLetterData = normaLetterData;
+        _selectedLetterData = selectedLetterData;
+        _correctLetterData = correctLetterData;
+
+        GetComponent<SpriteRenderer>().sprite = _normaLetterData.image;
+    }
+    private void OnMouseDown()
+    {
+        OnEnableSquareSelection();
+        GameEvent.EnableSquareSelectionMethod();
+        CheckSquare();
+        displayedImage.sprite = _selectedLetterData.image;
+    }
+
+    private void OnMouseEnter()
+    {
+        CheckSquare();
+    }
+    private void OnMouseUp()
+    {
+        GameEvent.ClearSelectionMethod();
+        GameEvent.DisableSquareSelectionMethod();
+    }
+
+    public void CheckSquare()
+    {
+        if (_selected == false && _clicked == true)
+        {
+            _selected = true;
+            GameEvent.CheckSquareMethod(_normaLetterData.letter, gameObject.transform.position, _index);
+        }
+    }
+
 }
--- Assets/Scripts/WordsGrid.cs
+++ Assets/Scripts/WordsGrid.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WordsGrid : MonoBehaviour
 {
     public GameData currentGameData;
     public GameObject gridSquarePrefab;
@@ -14,6 +14,56 @@ public class NewBehaviourScript : MonoBehaviour
 
     private List<GameObject> squareList = new List<GameObject>();
 
+    private void Start()
+    {
+        SpawnGridSquares();
+        SetSquaresPosition();
+    }
+    private void SetSquaresPosition()
+    {
+        var squareRect = squareList[0].GetComponent<SpriteRenderer>().sprite.rect;
+        var squareTransform = squareList[0].GetComponent<Transform>();
+        var offset = new Vector2
+        {
+            x = (squareRect.width * squareTransform.localScale.x + squareOffset) * 0.01f,
+            y = (squareRect.height * squareTransform.localScale.y + squareOffset) * 0.01f
+        };
+        var startPosition = GetFirstSquarePosition();
+        int columnNumber = 0;
+        int rowNumber = 0;
+        foreach (var square in squareList)
+        {
+            if (rowNumber + 1 > currentGameData.selectedBoardData.Rows)
+            {
+                columnNumber++;
+                rowNumber = 0;
+            }
+            var positionX = startPosition.x + offset.x * columnNumber;
+            var positionY = startPosition.y - offset.y * rowNumber;
+
+            square.GetComponent<Transform>().position = new Vector2(positionX, positionY);
+            rowNumber++;
+        }
+    }
+
+    private Vector2 GetFirstSquarePosition()
+    {
+        var startPosition = new Vector2(0f, transform.position.y);
+        var squareRect = squareList[0].GetComponent<SpriteRenderer>().sprite.rect;
+        var squareTransform = squareList[0].GetComponent<Transform>();
+        var squareSize = new Vector2(0f, 0f);
+
+        squareSize.x = squareRect.width * squareTransform.localScale.x;
+        squareSize.y = squareRect.height * squareTransform.localScale.y;
+
+        var midWidthPosition = (((currentGameData.selectedBoardData.Columns - 1) * squareSize.x) / 2) * 0.01f;
+        var midWidthHeight = (((currentGameData.selectedBoardData.Rows - 1) * squareSize.y) / 2) * 0.01f;
+
+        startPosition.x = (midWidthPosition != 0) ? midWidthPosition * -1 : midWidthPosition;
+        startPosition.y += midWidthHeight;
+
+        return startPosition;
+    }
     private void SpawnGridSquares()
     {
         if (currentGameData != null)
@@ -42,6 +92,10 @@ public class NewBehaviourScript : MonoBehaviour
                     else
                     {
                         squareList.Add(Instantiate(gridSquarePrefab));
+                        squareList[squareList.Count - 1].GetComponent<GridSquare>().SetSprite(normalLetterData, correctLetterData, selectedLetterData);
+                        squareList[squareList.Count - 1].transform.SetParent(this.transform);
+                        squareList[squareList.Count - 1].GetComponent<Transform>().position = new Vector3(0f, 0f, 0f);
+                        squareList[squareList.Count - 1].transform.localScale = squareScale;
                     }
 
                 }
